7cad81e872c98af70a2039f84affb46ebb44fae9
Goran Udovicic <v-gudovicic@microsoft.com>
2022-10-03 12:34:11 +0200

    nepotrebno

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -22,7 +22,6 @@ namespace WebApplication1.Controllers
         
         public IEnumerable<WeatherForecast> Get()
         {
-            WeatherForecast weatherForecast = new();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
